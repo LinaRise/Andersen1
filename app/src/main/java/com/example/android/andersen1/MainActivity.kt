202ae2c0e2f687587b55d51a5ba3c02dd567d36a
Lina <44583511+LinaRise@users.noreply.github.com>
2021-07-17 13:14:19 +0300

    main activity description

--- app/src/main/java/com/example/android/andersen1/MainActivity.kt
+++ app/src/main/java/com/example/android/andersen1/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.android.andersen1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Main activity
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
